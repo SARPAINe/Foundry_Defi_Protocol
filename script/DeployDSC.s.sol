7340c6a12ca89e7239b29edd4bdbfcc3deb286b0
shaharin <ahmedshaharin7589@gmail.com>
2025-07-02 15:08:17 +0600

    feat: add deployment and mock scripts for DSCEngine testing
    - Introduce `DeployDSC` script to facilitate the deployment of DecentralizedStableCoin and DSCEngine contracts.
    - Add `HelperConfig` script to manage network configurations including price feeds and token addresses.
    - Implement `ERC20Mock` contract for simulating ERC20 tokens in test environments with minting, burning, and internal transfer functionalities.
    - Create `MockV3Aggregator` contract to mock Chainlink price feeds, allowing tests to simulate price data.
    - These additions enable comprehensive testing and simulation of the DSC ecosystem across different network setups.

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (DecentralizedStableCoin dsc, DSCEngine dscEngine) {
+        HelperConfig config = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            config.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+        vm.startBroadcast(deployerKey);
+        // Deploy the DecentralizedStableCoin contract
+        dsc = new DecentralizedStableCoin();
+        console.log("DecentralizedStableCoin deployed at:", address(dsc));
+
+        // Deploy the DSCEngine contract
+        dscEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        console.log("DSCEngine deployed at:", address(dscEngine));
+
+        dsc.transferOwnership(address(dscEngine));
+
+        vm.stopBroadcast();
+
+        return (dsc, dscEngine);
+    }
+}
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address wethUsdPriceFeed;
+        address wbtcUsdPriceFeed;
+        address weth;
+        address wbtc;
+        uint256 deployerKey;
+    }
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2500 * 10e8;
+    int256 public constant WBTC_USD_PRICE = 107564 * 10e8;
+    int256 public defaultAnvilKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            // Sepolia
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory sepoliaNetworkConfig) {
+        sepoliaNetworkConfig = NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, // ETH / USD
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory anvilNetworkConfig) {
+        if (activeNetworkConfig.wethUsdPriceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+        ERC20Mock weth = new ERC20Mock("WETH", "WETH", msg.sender, 10 ** 18);
+
+        MockV3Aggregator wbtcUsdPriceFeed = new MockV3Aggregator(DECIMALS, WBTC_USD_PRICE);
+        ERC20Mock wbtc = new ERC20Mock("WBTC", "WBTC", msg.sender, 10 ** 8);
+
+        vm.stopBroadcast();
+
+        return NetworkConfig({
+            wethUsdPriceFeed: address(ethUsdPriceFeed),
+            wbtcUsdPriceFeed: address(wbtcUsdPriceFeed),
+            weth: address(weth),
+            wbtc: address(wbtc),
+            deployerKey: defaultAnvilKey
+        });
+        // anvilNetworkConfig = NetworkConfig({
+        //     wethUsdPriceFeed: address(wethUsdPriceFeed),
+        //     wbtcUsdPriceFeed: address(wbtcUsdPriceFeed),
+        //     weth: address(weth),
+        //     wbtc: address(wbtc),
+        //     deployerKey: vm.envUint("PRIVATE_KEY")
+        // });
+
+        // activeNetworkConfig = anvilNetworkConfig;
+    }
+}
